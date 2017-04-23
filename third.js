fed03df362c53595409c5eed823db429899ac56e
AseelM <aseelmustafa@outlook.com>
2017-04-23 18:52:37 +0100

    changed form code

--- third.js
+++ third.js
@@ -3,5 +3,5 @@ var frmvalidator  = new Validator("contactform");
 frmvalidator.addValidation("name","req","Please provide your name");
 frmvalidator.addValidation("email","req","Please provide your email");
 frmvalidator.addValidation("email","email",
-  "Please enter a valid email address");
+"Please enter a valid email address");
 </script>
